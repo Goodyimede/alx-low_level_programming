e100de31167b411792dae2a454324316189c0548
Goodyimede <imoelingoodness@gmail.com>
2023-09-14 16:17:29 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,7 +15,7 @@ void print_triangle(int size)
 	{
 		for (hash = 1; hash <= size; hash++)
 		{
-			for (index = size - hash; index > 0; index++)
+			for (index = size - hash; index > 0; index--)
 			{
 				_putchar(' ');
 			}
