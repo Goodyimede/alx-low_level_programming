4e9d54f777fb7e28e7b452ba1b41c6c95908088f
Goodyimede <imoelingoodness@gmail.com>
2023-09-18 15:32:37 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,6 +13,7 @@ int _atoi(char *s)
 	i = 0;
 	d = 0;
 	n = 0;
+	len = 0;
 	f = 0;
 	digit = 0;
 
@@ -31,6 +32,7 @@ int _atoi(char *s)
 		f = 1;
 		if (s[i + 1] < '0' || s[i + 1] > '9')
 			break;
+		f = 0;
 	}
 	i++;
 	}
