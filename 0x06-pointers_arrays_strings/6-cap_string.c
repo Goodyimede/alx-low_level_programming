94b4d9ffcb7fb3a3e197519130203fc88bae40a2
Goodyimede <imoelingoodness@gmail.com>
2023-09-21 03:37:52 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,26 +2,26 @@
 /**
  * cap_string - a function that capitalizes all words of a string
  *
- * @str: input string
+ * @str: string input
  *
- * Return: str the modified string
+ * Return: str, the modified string
  */
 char *cap_string(char *str)
 {
-	int j, k;
+	int i, j;
 	char c[] = {44, 59, 46, '!', '?', '"', '(', ')', '{', '}', ' ', '\t', '\n'};
 
-	for (j = 0; str[j] != '\0'; j++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (j == 0 && str[j] >= 'a' && str[j] <= 'z')
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 		{
-			str[j] = str[j] - 32;
+			str[i] = str[i] - 32;
 		}
-	for (k = 0; c[k] != '\0'; k++)
+	for (j = 0; c[j] != '\0'; j++)
 	{
-		if (c[k] == str[k] && (str[k + 1] >= 'a' && str[k + 1] <= 'z'))
+		if (c[j] == str[i] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
 		{
-			str[j + 1] = str[j + 1] - 32;
+			str[i + 1] = str[i + 1] - 32;
 		}
 	}
 	}
