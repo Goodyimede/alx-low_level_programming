1731ae43bfc00641478dac31aef0ec377c097cf8
Goodyimede <imoelingoodness@gmail.com>
2023-09-25 05:36:57 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - The main function
+ * @needle: substring
+ * @haystack: string
+ *
+ * Return: Always 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *j = haystack;
+		char *k = needle;
+
+		while (*j == *k && *k != '\0')
+		{
+			j++;
+			k++;
+		}
+
+		if (*k == '\0')
+			return (haystack);
+	}
+	return (0);
+}
