da7ad7c97056438228f6799887d22995da60dff8
Goodyimede <imoelingoodness@gmail.com>
2023-10-10 06:09:07 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-object_like_macro
+ *
+ * Desc: header file that defines a macro named size
+ */
+
+#define SIZE 1024
+
+#endif
