1a70d4dbd02bf37d469e2157386fd255ed7f77ff
Goodyimede <imoelingoodness@gmail.com>
2023-10-10 06:37:51 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTION_LIKE_MACROLIKE_H
+#define FUNCTION_LIKE_MACROLIKE_H
+
+/**
+ * File: 3-function_like_macro
+ *
+ * @x: input (integer)
+ *
+ * Desc: header file that compute absolute value
+ */
+
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
